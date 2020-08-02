d6df6c67214a729268985d135d911c1c3fcaab49
jason133809 <369922148@qq.com>
2020-08-02 17:20:22 +0800

    homework for this weekk

---
+++ Week_01/caltwosum.py
@@ -0,0 +1,22 @@
+﻿class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+
+
+def main():
+    nums=[3,2,4]
+    target=6
+    t=Solution()
+    l=t.twoSum(nums,target)
+    print(l)
+    
+if __name__=="__main__":
+    main()
---
+++ Week_01/thiftzero.py
@@ -0,0 +1,13 @@
+﻿class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        j=0 #替换下标从0开始
+        for i in range(len(nums)):
+            if nums[i]!=0:  #遍历数组 如果第一次碰到不为0的值，交换位置 
+                nums[j],nums[i]=nums[i],nums[j]
+                print(nums[j],nums[i])
+                j=j+1 #替换下标加1 便于存储第二次交换的值
+        return nums
