4cbd59081dc31f5b2a2e5b2893a75580282d5fbe
jason133809 <43096123+jason133809@users.noreply.github.com>
2020-08-10 00:41:50 +0800

    Add files via upload
    上传第二周作业

---
+++ Week_02/二叉树的前序遍历.py
@@ -0,0 +1,36 @@
+﻿#
+# @lc app=leetcode.cn id=144 lang=python
+#
+# [144] 二叉树的前序遍历
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+        
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        ens = []
+        if root == None:
+            return ens
+        self.preTraverse(root, ens)
+        return ens
+    def preTraverse(self,t, ens):
+        ens.append(t.val)
+        if t.left:
+            self.preTraverse(t.left, ens)
+        if t.right:
+            self.preTraverse(t.right, ens)
+
+            
+# @lc code=end
+
---
+++ Week_02/有效的字母异位词.py
@@ -0,0 +1,33 @@
+﻿# @lc app=leetcode.cn id=242 lang=python
+#
+# [242] 有效的字母异位词
+#
+
+# @lc code=start
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if s==None or t==None or s=="" or t=="":
+            return False
+        if len(s)!=len(t):
+            return False
+        else:
+            s_dict={}
+            t_dict={}
+            for i in range(len(s)):
+                if s_dict.get(s[i])==None:
+                    s_dict[s[i]]=1
+                else:
+                    s_dict[s[i]]=s_dict.get(s[i])+1
+                if t_dict.get(t[i])==None:
+                    t_dict[t[i]]=1
+                else:
+                    t_dict[t[i]]=t_dict.get(t[i])+1
+        for key in s_dict.keys():
+            if s_dict.get(key)!=t_dict.get(key):
+                return False
+        return True
